33e1e6b0ebd0b64f6dc1d9d906b8e4f21d0cb633
jemappellemack <jemappellemack@gmail.com>
2020-12-20 00:10:45 +0900

    fix(KintaiApplyModal): close modal if click outside

--- frontend/src/components/KintaiApplyModal.tsx
+++ frontend/src/components/KintaiApplyModal.tsx
@@ -30,6 +30,7 @@ const KintaiApplyModal: FC<KintaiApplyModalProps> = ({
   return (
     <Modal
       show={show}
+      onHide={onHide}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered>
